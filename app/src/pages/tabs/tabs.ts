cee1b767bfcf6007499655d209418590e0aad0b2
shengliang <ahliang51@hotmail.com>
2018-03-31 11:53:22 +0800

    added splash screen

--- app/src/pages/tabs/tabs.ts
+++ app/src/pages/tabs/tabs.ts
@@ -4,6 +4,8 @@ import { HomePage } from '../home/home';
 import { SuperTabsController } from 'ionic2-super-tabs';
 import { CartPage } from '../cart/cart';
 import { ProfilePage } from '../profile/profile';
+import { ProfileProvider } from '../../providers/profile/profile';
+import { Storage } from '@ionic/storage';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -13,14 +15,21 @@ export class TabsPage {
   tab1Root = HomePage;
   tab2Root = CartPage;
   tab3Root = ProfilePage;
+  storeCredit;
 
   pageTitle = "Home";
 
-  constructor(private superTabsCtrl: SuperTabsController) {
+  constructor(private superTabsCtrl: SuperTabsController,
+    private storage: Storage,
+    private profileService: ProfileProvider) {
   }
 
   ngAfterViewInit() {
-
+    this.storage.get('token').then(result => {
+      this.profileService.retrieveProfile(result).subscribe(data => {
+        this.storeCredit = data.result.store_credit;
+      })
+    })
     // must wait for AfterViewInit if you want to modify the tabs instantly
     // this.superTabsCtrl.setBadge('homeTab', 5);
   }
@@ -34,6 +43,13 @@ export class TabsPage {
   }
 
   onTabSelect(ev: any) {
+    this.storage.get('token').then(result => {
+      this.profileService.retrieveProfile(result).subscribe(data => {
+        this.storeCredit = data.result.store_credit;
+      })
+
+    })
+
     console.log('Tab selected', 'Index: ' + ev.index, 'Unique ID: ' + ev.id);
     if (ev.index == 0) {
       this.pageTitle = "Home"
